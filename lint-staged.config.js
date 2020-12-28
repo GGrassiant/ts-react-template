530adbea8da630bf0775be5ad7a1271ab0c95db7
ggrassiant <guillaume.grassiant@gmail.com>
2020-12-28 13:13:25 -0500

    try npm run coverage on lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   '**/*.ts?(x)': () => 'npm run type-check',
   '**/*.(ts|js)?(x)': (filenames) => `npm run lint ${filenames.join(' ')}`,
-  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['prettier --write'],
+  'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': [
+    'prettier --write',
+    'npm run test:coverage',
+  ],
 };
